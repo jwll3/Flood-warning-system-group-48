2996e29985cbc34acdba5135da2955fc09707d3c
Jamie Lai <jwll3@cam.ac.uk>
2022-02-04 22:06:54 +0000

    defined functions for 1C and 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,15 @@ geographical data.
 
 """
 
+def stations_within_radius(stations, centre, r):
+    """returns a list of all stations (type MonitoringStation) within the radius r, of centrepoint coordinate x"""
+
+
 from .utils import sorted_by_key  # noqa
+
+
+
+# Task 1D also goes below in this section
+
+def rivers_with_station(stations): 
+    """"""
