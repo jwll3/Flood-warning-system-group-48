885aeedf34065386eb919196b4f09f9cc8b917da
Jamie Lai <jwll3@cam.ac.uk>
2022-02-06 22:46:22 +0000

    alphabetically sorted rivers

--- Task1D.py
+++ Task1D.py
@@ -4,6 +4,13 @@ from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 
 #print(rivers_with_station(stations))
-print ("dictionary:")
-print(station_by_river(stations)) 
+#print ("dictionary:")
+#print(station_by_river(stations)) 
+
+
+no_of_stations_with_river = len(rivers_with_station(stations))
+rivers_list = list(rivers_with_station(stations))
+alphabetical_rivers = sorted(rivers_list) 
+print("test ")
+print(alphabetical_rivers)
 
