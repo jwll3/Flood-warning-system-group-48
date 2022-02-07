9722b7ceee8296f8610385fc247fa6bf7e52653d
Jamie Lai <jwll3@cam.ac.uk>
2022-02-07 00:35:12 +0000

    trying to get isnull to work for 1F

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -7,6 +7,9 @@ for manipulating/modifying station data
 """
 
 
+from pandas import isnull
+
+
 class MonitoringStation:
     """This class represents a river level monitoring station"""
 
@@ -38,3 +41,9 @@ class MonitoringStation:
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
         return d
+"""
+    def typical_range_consistent(self):
+       return isnull(d) 
+
+def inconsistent_typical_range_stations(stations): 
+"""    
