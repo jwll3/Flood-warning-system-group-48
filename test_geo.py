23b12c3cb95860d1d1b89b3768d94f2f90843402
Jamie Lai <jwll3@cam.ac.uk>
2022-02-08 21:34:05 +0000

    test station by river

--- test_geo.py
+++ test_geo.py
@@ -5,6 +5,7 @@ from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 
 
+"""Tests for 1D"""
 def test_rivers_with_station():
     """test that rivers_with_station only returns each river once"""
     #turn the rivers_with_station set list  
@@ -14,5 +15,7 @@ def test_rivers_with_station():
     #test if the lengh of the list and set are the same (if they are the same- no duplicate entries)
     assert len(rivers_list) == len(rivers_set)
 
-
-    
+def test_station_by_river(): 
+    """test if station_by_river is callable"""
+    x= station_by_river(stations)
+     
