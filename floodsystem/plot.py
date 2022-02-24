007f228c469e1317b221159d547c3f7708dd1ca9
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2022-02-24 01:49:33 +0000

    Stephen1.6

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,15 +3,36 @@ from .datafetcher import fetch_measure_levels
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-
 def plot_water_levels(station, dates, levels):
-    t = [datetime(2016, 12, 30), datetime(2016, 12, 31), datetime(2017, 1, 1),
+
+
+    # Fetch data over past 2 days
+    dt = 2
+    dates, levels = fetch_measure_levels(
+        station.measure_id, dt=timedelta(days=dt))
+
+    # Print level history
+    for date, level in zip(dates, levels):
+        print(date, level)
+
+
+    #dt = 2
+    dates, levels = fetch_measure_levels(
+        station.measure_id, dt=timedelta(days=dt))
+
+    # Print level history
+    for date, level in zip(dates, levels):
+        print(date, level)
+
+
+
+    tt = [datetime(2016, 12, 30), datetime(2016, 12, 31), datetime(2017, 1, 1),
         datetime(2017, 1, 2), datetime(2017, 1, 3), datetime(2017, 1, 4),
         datetime(2017, 1, 5)]
     level = [0.2, 0.7, 0.95, 0.92, 1.02, 0.91, 0.64]
 
     # Plot
-    plt.plot(t, level)
+    plt.plot(tt, level)
 
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
