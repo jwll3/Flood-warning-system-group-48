3073bea5322e7ea2ecc10ac0c4a3b81053bb3b21
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2022-02-25 20:39:30 +0000

    TaskB&Efinished

--- Task2E.py
+++ Task2E.py
@@ -1,17 +1,41 @@
 from floodsystem.plot import plot_water_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_level_over_threshold
+import datetime
 
 def run():
     """Requirements for Task2E"""
     # Build list of stations
     stations = build_station_list()
-
-    # Station name to find
-    station_name = "Cam"
+    update_water_levels(stations)
 
 
-    plot_water_levels(stations[0], 3, 5)
+    stations_list = stations_level_over_threshold(stations, 0.8)
+
+    selected_stations = stations_list[:5]
+
+    measure_ids = []
+
+    for i in range(0, 5):
+        measure_ids.append(stations_list[i][0].measure_id)
+
+    dt = 10
+    dt = datetime.timedelta(days=dt)
+    measure_id = measure_ids[0]
+
+    counter = 0
+
+    #dates, levels = fetch_measure_levels(measure_id, dt)
+    #plot_water_levels(stations[0], dates, levels)
+
+    for thing in selected_stations:
+        dates, levels = fetch_measure_levels(selected_stations[counter][0].measure_id, dt)
+        plot_water_levels(selected_stations[counter][0], dates, levels)
+        counter = counter + 1
+
+
+
 
 
 if __name__ == "__main__":
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -5,17 +5,19 @@ from floodsystem.utils import sorted_by_key
 
 
 def stations_level_over_threshold(stations, tol):
-    """Returns list of tuples of the station object and its relative water level, in descending order, if the """
+    """Returns list of tuples of the station object and its relative water level, in descending order"""
     list_to_return = []
     for station in stations:
-        if station.relative_water_level() >= tol:
+        if station.relative_water_level() is None:
+            pass
+        elif station.relative_water_level() >= tol:
             new_tuple = (station, station.relative_water_level())
             list_to_return.append(new_tuple)
 
         else:
             pass
 
-    return list_to_return
+    return sorted_by_key(list_to_return, 1, reverse=True)
 
 
 def stations_highest_rel_level(stations, N): 
