49734d6c8e0232b242fe6ad3ac7175ebb8d931bd
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2022-03-03 13:27:50 +0000

    TaskG_done

--- Task2G.py
+++ Task2G.py
@@ -1,3 +1,4 @@
+#from curses import KEY_A1
 from floodsystem.stationdata import *
 from floodsystem.analysis import *
 from floodsystem.flood import *
@@ -13,84 +14,140 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     
-    #calculate the total number of consistent stations 
-    Number_of_consistent_stations = len(stations) - len(inconsistent_typical_range_stations(stations))
-
-    #print(stations_level_over_threshold(stations, 0)) 
-    All_valid_stations = stations_level_over_threshold(stations, -1000)
-
-    #valid stations name with relative water level 
-    valid_station_name_with_rel_level = stations_highest_rel_level(stations, len(All_valid_stations)-200) 
-    print (valid_station_name_with_rel_level)
-    print (len(valid_station_name_with_rel_level))
-
-    "ABOVE IS A REALLY BODGED WAY TO GET A LIST OF ALL STATIONS ALONG WITH THEIR RELATIVE LEVEL IN A LIST OF TUPLES. "
-    "IT WOULD BE GOOD IF YOU COULD FIND A BETTER SYSTEM THAT DIDNT USE A -66 AT THE END TO REACH THE LIMIT"
-
     #set up stations and dates for functions 
     stations_list = stations_level_over_threshold(stations, -1000)
-    selected_stations = stations_list
+    selected_stations = stations_list[0:200]
     dt = 2
-    dt = datetime.timedelta(days=dt)
+    dt = datetime.timedelta(hours=dt)
 
+    ##Create a list of tuples: station name and risk factor
+    stations_risk_factor = []
+    stations_risk_grading = []
 
-
-    "this bit works!"
-    #this for loop is going to go through every station, and work out the rate of water level rise through backward integration
+    #this for loop is going to go through every station, and work out the rate of water level rise through backward differentiation
     #for i in range(len(valid_station_name_with_rel_level)):
-    for i in range(len(100)):
+    for i in range(len(selected_stations)):
         try:
-            #initial data needed for functions 
-            dates, levels = fetch_measure_levels(selected_stations[i+1][0].measure_id, dt)
-            station = selected_stations[i+1]
 
-            #turn dates into numerical values 
-            float_dates = matplotlib.dates.date2num(dates)
-            t = np.linspace(float_dates[0], float_dates[-1], len(levels))
-            #Find the time of latest reading and the one before that, accounting for shift to avoid "poorly conditioned warnings"
-            latest_time_on_graph = t[-1] -t[0]
-            one_step_back = (t[-1] -t[0]) - (t[-2]-t[0]) 
-            #two_step_back = (t[-1] -t[0]) - (t[-3] -t[0]) for example if you wanted a larger step back 
-            # arbitary step of 1 is one day 
-            #one step back is equivelent to rate in water level increase between this reading and the last 
+            def find_rate_of_increase():
+                #initial data needed for functions 
+                dates, levels = fetch_measure_levels(selected_stations[i][0].measure_id, dt)
+                station = selected_stations[i]
+
+                #turn dates into numerical values 
+                float_dates = matplotlib.dates.date2num(dates)
+                t = np.linspace(float_dates[0], float_dates[-1], len(levels))
+                #Find the time of latest reading and the one before that, accounting for shift to avoid "poorly conditioned warnings"
+                latest_time_on_graph = t[-1] -t[0]
+                one_step_back = (t[-1] -t[0]) - (t[-2]-t[0]) 
+                #two_step_back = (t[-1] -t[0]) - (t[-3] -t[0]) for example if you wanted a larger step back 
+                # arbitary step of 1 is one day 
+                #one step back is equivelent to rate in water level increase between this reading and the last 
+            
+                #work out the polyfit poly1d object for the polynomial 
+                coefficients = polyfit(dates,levels,4)
+                #print (coefficients) 
+
+                #extract just the coefficients of x^4, x^3 ect. from poly1d object 
+                terms = coefficients[0]
+                #ie terms[0]) is the constant 
+                #ie terms[3] is the x^3 coefficient 
+
+                #create a function for the model polynomial 
+                def Polynomial_function(x):
+                    return terms[4]*x*x*x*x + terms[3]*x*x*x + terms[2]*x*x + terms[1]*x + terms[0]
         
-            #work out the polyfit poly1d object for the polynomial 
-            coefficients = polyfit(dates,levels,4)
-            print (coefficients) 
+                #f(a)
+                First_term = Polynomial_function(latest_time_on_graph)
+                #f(a-h)
+                Second_term = Polynomial_function(latest_time_on_graph-one_step_back)
+                #h = one_step_back
 
-            #extract just the coefficients of x^4, x^3 ect. from poly1d object 
-            terms = coefficients[0]
-            #ie terms[0]) is the constant 
-            #ie terms[3] is the x^3 coefficient 
+                def Numerical_backward_differentiation( first, second, h): #value of h is size of step for backward differentiation
+                    return ((first-second) / h) 
 
-            #create a function for the model polynomial 
-            def Polynomial_function(x):
-                return terms[4]*x*x*x*x + terms[3]*x*x*x + terms[2]*x*x + terms[1]*x + terms[0]
-    
+                rate_of_water_level_rise = Numerical_backward_differentiation (First_term, Second_term, one_step_back) # gives a value of rise in water level per day accoring to latest reading and the one right before it 
 
-            "For numerical backwards integration : (f(a) - f(a - h))/h, where f is polynomial function, a is latest_time_on_graph and h is one_step_back"
+                #print(selected_stations[i][0].name)
+                #print (rate_of_water_level_rise) 
 
-            #f(a)
-            First_term = Polynomial_function(latest_time_on_graph)
-        
-            #f(a-h)
-            Second_term = Polynomial_function(latest_time_on_graph-one_step_back)
-        
-            #h = one_step_back
+                return rate_of_water_level_rise
 
-            def Numerical_backward_integration( first, second, h): #value of h is size of step for backward integration
-                return ((first-second) / h) 
+            
 
-            rate_of_water_level_rise = Numerical_backward_integration (First_term, Second_term, one_step_back) # gives a value of rise in water level per day accoring to latest reading and the one right before it 
+            ##Calculate the risk factor and append to the list.
+            K_1 = 10
+            K_2 = 1
 
-            print (rate_of_water_level_rise) 
+            #risk_factor = (K_1 * rate_of_water_level_rise) + (K_2 * selected_stations[i][0].relative_water_level())
+            risk_grading = "Undetermined"
 
-            "can you put water level rise into the tuple of the station"
-            "i.e so you have [(station name, rel water level, rate of water level rise) (station name, rel water level, rate of water level rise)...]" 
-            "sorry, would have done this bit myself but i couldnt figure out how to get all teh stations into the list (i have a bodged attemp above) "
+            ##Determine the risk gradings
+
+            if selected_stations[i][0].relative_water_level() <= 0.8:
+                risk_grading = "No Risk"
+                
+
+
+            elif (selected_stations[i][0].relative_water_level() > 0.8):
+                rate_of_water_level_rise = find_rate_of_increase()
+                water_level = selected_stations[i][0].relative_water_level()
+                risk_factor = (K_1 * rate_of_water_level_rise) + (K_2 * water_level)
+
+                #print(selected_stations[i][0].name)
+                #print(rate_of_water_level_rise)
+                #print(water_level)
+                #print(risk_factor)
+
+                if (risk_factor >= 10) and (rate_of_water_level_rise > 0):
+                    risk_grading = "severe"
+
+                elif (risk_factor >= 10) and (rate_of_water_level_rise < 0):
+                    risk_grading = "high"
+
+                elif (2 <= risk_factor <= 10) and (rate_of_water_level_rise > 0):
+                    risk_grading = "high"
+
+                elif (2 <= risk_factor <= 10) and (rate_of_water_level_rise < 0):
+                    risk_grading = "moderate"
+
+                elif (1.5 <= risk_factor <= 2) and (rate_of_water_level_rise > 0):
+                    risk_grading = "moderate"
+
+                elif (1.5 <= risk_factor <= 2) and (rate_of_water_level_rise < 0):
+                    risk_grading = "low"
+
+                elif (risk_factor <= 1.5) and (rate_of_water_level_rise > 0):
+                    risk_grading = "low"
+
+                elif (risk_factor <= 2) and (rate_of_water_level_rise < 0):
+                    risk_grading = "No Risk"
+
+
+
+            #factor_tuple_to_append = (selected_stations[i][0].name, risk_factor)
+            #stations_risk_factor.append(factor_tuple_to_append)
+
+            if risk_grading == "No Risk":
+                pass
+            else: 
+                grading_tuple_to_append = (selected_stations[i][0].name, risk_grading)
+                stations_risk_grading.append(grading_tuple_to_append)
+
+            
+            
+            #print(stations_risk_factor)
+
+            #plot_water_levels(selected_stations[i][0], dates, levels)
 
         except:
             pass
+    
+    print(stations_risk_grading)
+
+
+
+
 
 
 
